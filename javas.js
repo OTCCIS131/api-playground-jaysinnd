93d55a6ed4e2ab11421b78eb4ac29eac9361dd4e
Unknown <jdeansart@gmail.com>
2017-11-13 09:14:57 -0600

    working now.

--- javas.js
+++ javas.js
@@ -2,38 +2,19 @@ $(function() {
     const vm = new Vue({
         el: '#app',
         data: {
-            loading: false,
-            showing: 'dogs',
-            showing: 'breeds',
-            all: [],
-            breed: [],
-            dog: null
-
+            dogs: [],
             
         },
         methods: {
             loadDogs() {
                 this.loading = true
-                this.$http.get('https://dog.ceo/api/breeds/list/all')
+                this.$http.get('https://dog.ceo/api/breeds/list')
                     .then(resp => {
-                        this.all = resp.body
-                        this.loading = false
+                        this.dogs = resp.body.message
+                        
                     })
             },
-            selectDog(dog) {
-                this.dog = dog
-                this.showing = 'breeds'
-                this.loadBreeds()
-            },
-            loadBreeds() {
-                this.breeds = []
-                this.dogs.breeds.array.forEach(breed => {
-                    this.$http.get(breed)
-                        .then(resp => {
-                            this.breed.push(resp.body)
-                        })
-                })
-            }
+
         }
     })
 })
